1eee9b431c349ba244bafee469430c1798f2e50e
angelika <161327272+Tamayo-Angelika-BSCS32E1@users.noreply.github.com>
2024-04-13 02:27:28 +0800

    home page landing

---
+++ loginregister/Models/HomePageModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace loginregister.Models
+{
+    public class HomePageModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- loginregister/obj/Debug/net8.0/loginregister.AssemblyInfo.cs
+++ loginregister/obj/Debug/net8.0/loginregister.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6392d5f3cd1ee800339824b8749edb1ff8b44ece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4339c7fee029e6498edbca00f9cd91291d8b62ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyTitleAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
