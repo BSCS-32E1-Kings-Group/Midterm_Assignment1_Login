e38fadf17e34225f27e9b9e01d6359f7136fa6a5
Adante-Stephen-BSCS32E1 <stphnadante@gmail.com>
2024-04-14 22:25:23 +0800

    login function

--- loginregister/obj/Debug/net8.0/loginregister.AssemblyInfo.cs
+++ loginregister/obj/Debug/net8.0/loginregister.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e230de626f689e19044bc640bdd77842ff8d5ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fe9dbf3353190ac7bf1f7b850cdd6b8ace0a9ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyTitleAttribute("loginregister")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
